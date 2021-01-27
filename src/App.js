85f5797cb7f5dd40516651c8edc34d543c932d28
Mohamed Eldeeb <41117194+moeldeeb98@users.noreply.github.com>
2021-01-27 20:55:48 +0200

    init the project on sandbox env

--- src/App.js
+++ src/App.js
@@ -15,66 +15,21 @@ function Link(props) {
 
 function App() {
   return (
-    <View style={styles.app}>
-      <View style={styles.header}>
-        <Image
-          accessibilityLabel="React logo"
-          source={{ uri: logoUri }}
-          resizeMode="contain"
-          style={styles.logo}
-        />
-        <Text style={styles.title}>React Native for Web</Text>
-      </View>
+    <View>
       <Text style={styles.text}>
-        This is an example of an app built with{" "}
-        <Link href="https://github.com/facebook/create-react-app">
-          Create React App
-        </Link>{" "}
-        and{" "}
-        <Link href="https://github.com/necolas/react-native-web">
-          React Native for Web
-        </Link>
+        This is an example of an app built in react native
       </Text>
-      <Text style={styles.text}>
-        To get started, edit{" "}
-        <Link href="https://codesandbox.io/s/q4qymyp2l6/" style={styles.code}>
-          src/App.js
-        </Link>
-        .
-      </Text>
-      <Button onPress={() => {}} title="Example button" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  app: {
-    marginHorizontal: "auto",
-    maxWidth: 500
-  },
-  logo: {
-    height: 80
-  },
-  header: {
-    padding: 20
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: "1.5rem",
-    marginVertical: "1em",
-    textAlign: "center"
-  },
   text: {
     lineHeight: "1.5em",
     fontSize: "1.125rem",
     marginVertical: "1em",
-    textAlign: "center"
-  },
-  link: {
-    color: "#1B95E0"
-  },
-  code: {
-    fontFamily: "monospace, monospace"
+    textAlign: "center",
+    color: "#f00"
   }
 });
 
